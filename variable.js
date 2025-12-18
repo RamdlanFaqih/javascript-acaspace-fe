784a6be9b3e9286f41873e20ab32db0fe216282b
RamdlanFaqih <ramfaqih@gmail.com>
2025-12-18 20:58:12 +0700

    feat: variables

---
+++ variable.js
@@ -0,0 +1,29 @@
+//variable => tempat penyimpanan / container
+// sebuah nilai / value (data)
+// tipe data
+
+// deklarasi variable
+// keyword : var
+var gelas = "air teh"
+// reassign var
+// gelas = "air kopi"
+
+var gelas = "air kopi"
+
+
+// keyword: let
+let piring = "nasi"
+// reassign let
+// piring = "buah buahan"
+// let piring = "buah buahan"
+
+// keyword: const
+const usia = 21 //data from database
+// reassign const
+// usia = 22
+usia = 22
+
+
+// console.log("gelas isinya", gelas)
+// console.log("piring diatasnya", piring)
+console.log("usia budi", usia)
