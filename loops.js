1e3dbb7507fb13d69eba0b53a55764b367cdd231
RamdlanFaqih <ramfaqih@gmail.com>
2025-12-20 11:13:08 +0700

    feat: loops

---
+++ loops.js
@@ -0,0 +1,25 @@
+//looping / iteration
+
+// for loop statement
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// while loop
+// let a = 11;
+// while (a <= 10) {
+//   console.log(a);
+//   //   reassing variable
+//   //   a = a + 1
+//   // a += 1
+//   a++;
+// }
+
+// do while
+let a = 1;
+do {
+  console.log(a);
+  console.log("code nya jalan")
+  a++
+} while (a <= 10);
